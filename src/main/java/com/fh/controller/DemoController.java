2a22d7966ec758f651a6584a3c5d3e5f38c6d1e4
lwzbc <1289803520@qq.com>
2020-11-19 16:25:37 +0800

    第二次提交

--- src/main/java/com/fh/controller/DemoController.java
+++ src/main/java/com/fh/controller/DemoController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @RequestMapping("test")
     public      String   test(){
-        return "success";
+        return "success======";
+    }
+    @RequestMapping("test1")
+    public      String   test1(){
+        return "error";
     }
 }
